8297ed1acd3f8b5fb0b7f1f4958efd4b95531e04
fuyou4546 <52872288+fuyou4546@users.noreply.github.com>
2020-07-10 23:01:36 +0800

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -102,7 +102,7 @@ void block::FlagNumber(){
         }
     }
 }
-void block::Click(int i, int j){
+void block::Click0(int i, int j){
     //如果点击到为0的方格，就采用递归方法显示空白方格和空白方格周围的带有数字的方格
     //TODO:实现鼠标的在地图【i】【j】处的点击
     //TODO如果点到空白的格子就递归显示方块
@@ -110,61 +110,100 @@ void block::Click(int i, int j){
     //大体思路是，如果一个块是0就加100，对他周围每一个不等于100的块进行递归，
     //并让被重复运算的块的值始终在100~108
     //如果数字在1~8或99都翻开（加100），已经插了旗的不做操作
+
     if((GameMap[i][j]>=1 && GameMap[i][j]<8) || GameMap[i][j]==99)
     {
         GameMap[i][j]+=100;
     }
-
     if(GameMap[i][j]==0)
     {
         GameMap[i][j]+=100;
         if((i-1)>=0 && (j-1)>=0 && GameMap[i-1][j-1]!=100)
         {
-            Click(i-1,j-1);
+            Click0(i-1,j-1);
             GameMap[i-1][j-1]+=100;
             if(GameMap[i-1][j-1]>=150) GameMap[i-1][j-1]-=100;
         }
         if((i-1)>=0 && GameMap[i-1][j]!=100)
         {
-            Click(i-1,j);
+            Click0(i-1,j);
             GameMap[i-1][j]+=100;
             if(GameMap[i-1][j]>=150) GameMap[i-1][j]-=100;
         }
         if((i-1)>=0 && (j+1)<18 && GameMap[i-1][j+1]!=100)
         {
-            Click(i-1,j+1);
+            Click0(i-1,j+1);
             GameMap[i-1][j+1]+=100;
             if(GameMap[i-1][j+1]>=150) GameMap[i-1][j+1]-=100;
         }
         if((j-1)>=0 && GameMap[i][j-1]!=100)
         {
-            Click(i,j-1);
+            Click0(i,j-1);
             GameMap[i][j-1]+=100;
             if(GameMap[i][j-1]>=150) GameMap[i][j-1]-=100;
         }
         if((j+1)<18 && GameMap[i][j+1]!=100)
         {
-            Click(i,j+1);
+            Click0(i,j+1);
             GameMap[i][j+1]+=100;
             if(GameMap[i][j+1]>=150) GameMap[i][j+1]-=100;
         }
         if((i+1)<14 && (j-1)>=0 && GameMap[i+1][j-1]!=100)
         {
-            Click(i+1,j-1);
+            Click0(i+1,j-1);
             GameMap[i+1][j-1]+=100;
             if(GameMap[i+1][j-1]>=150) GameMap[i+1][j-1]-=100;
         }
         if((i+1)<14 && GameMap[i+1][j]!=100)
         {
-            Click(i+1,j);
+            Click0(i+1,j);
             GameMap[i+1][j]+=100;
             if(GameMap[i+1][j]>=150) GameMap[i+1][j]-=100;
         }
         if((i+1)<14 && (j+1)<18 && GameMap[i+1][j+1]!=100)
         {
-            Click(i+1,j+1);
+            Click0(i+1,j+1);
             GameMap[i+1][j+1]+=100;
             if(GameMap[i+1][j+1]>=150) GameMap[i+1][j+1]-=100;
         }
     }
+}
+//原来的变成Click0，现在能实现点击已翻开的部分，监测周围标旗的数和雷数是否相同，如果相同将周围的棋格翻开
+void block::Click(int i, int j)
+{
+    if(GameMap[i][j]>=101 && GameMap[i][j]<=108)
+    {
+        int mine0 = 0;
+        int flag0_true = 0;
+        int flag0_false = 0;
+        for(int x=-1;x<2;x++)
+        {
+            for(int y=-1;y<2;y++)
+            {
+                if(i+x>=0 && j+y>=0 && i+x<14 && j+y<18)
+                {
+                    if(GameMap[i+x][j+y]==99)
+                        mine0++;
+                    if(GameMap[i+x][j+y]==149)
+                        flag0_true++;
+                    if(GameMap[i+x][j+y]>=51 && GameMap[i+x][j+y]<=58)
+                        flag0_false++;
+                }
+            }
+        }
+        if(mine0 == flag0_false)
+        {
+            for(int x=-1;x<2;x++)
+            {
+                for(int y=-1;y<2;y++)
+                {
+                    if(i+x>=0 && j+y>=0 && i+x<14 && j+y<18)
+                    {
+                        Click0(i+x,j+y);
+                    }
+                }
+            }
+        }
+    }
+    Click0(i,j);
 }
